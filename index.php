2e810d096a6f2c1833a509da5446c1d8ede67d24
Sean Hayes <hayessean23@gmail.com>
2023-11-14 16:37:24 +0000

    First Index 1.0
    This the first look at the index page

---
+++ index.php
@@ -0,0 +1,68 @@
+
+<html>
+   <head>
+      <title>Connect to MariaDB Server</title>
+        <link rel="stylesheet" type="text/css" href="border.css" />
+      
+   </head>
+
+   <body>
+      <?php
+
+      /*if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+      $uri = 'https://';
+   } else {
+      $uri = 'http://';
+   }
+   $uri .= $_SERVER['HTTP_HOST'];
+   header('Location: '.$uri.'/dashboard/');
+   exit;*/
+
+         $dbhost = 'localhost';
+         $dbuser = 'sean';
+         $dbpass = 'metalislife';
+         $db = "metal_music";
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+      
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+         
+         //echo 'Connected successfully';
+      ?>
+
+     
+
+         <table border="" align="center">
+         <tr>
+         <td>Genre Name</td>
+         <td>Date of Origin</td>
+         <td>Place(s) of Origin</td>
+         <td>Notable Bands</td>
+         <td>Comments</td>
+         </tr>
+
+        <?php
+
+         $query = mysqli_query($conn, "SELECT * FROM genres")
+            or die (mysqli_error( $conn));
+
+             echo'<a href="test.html"><b> HOME</b> </a>';
+
+
+               while ($row = mysqli_fetch_array($query)) {
+                  echo
+                  "<tr>
+                  <td>{$row['GenreName']}</td>
+                  <td>{$row['DateofOrigin']}</td>
+                  <td>{$row['PlacesofOrigin']}</td>
+                  <td>{$row['NotableBands']}</td>
+                  <td>{$row['Comments']}</td>
+                  </tr\n>";
+               }
+
+         mysqli_close($conn);
+
+      ?>
+   </body>
+</html>
